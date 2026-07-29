f7c57310c017febd4f0ce24e65b0a66377bf124f
David Nilo <43155042+bboy22dxdavid@users.noreply.github.com>
2026-07-29 15:39:21 -0300

    foi criado arq para resovelr ex: sucessor de um numero. Para treinar sintaxe

---
+++ src/exercicio/ExercicioSucessor.java
@@ -0,0 +1,20 @@
+package exercicio;
+import java.util.Scanner;
+
+
+public class ExercicioSucessor {
+    static void main() {
+        // Exercicio Leia um número e exiba seu sucessor.
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite uma Numero: ");
+        int numero = scanner.nextInt();
+        int sucessor =   numero++;
+
+        //operador ternario
+        String resultado =  numero<= 0 ? "Digite um numero valido! " : "O Sucesso de " + sucessor + " e: " + numero;
+
+        System.out.println(resultado);
+    }
+}
